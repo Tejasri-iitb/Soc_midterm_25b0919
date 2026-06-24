c5c1fcbd1f0dfc0ca77cc064bc2cd78e82c28713
Tejasri-iitb <25b0919@iitb.ac.in>
2026-06-24 23:49:36 +0530

    added sasha_and_the_beautiful_array.cpp

---
+++ Week4/sasha_and_the_beautiful_array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin>>a[i];
+    }
+    
+    sort(a.begin(), a.end());
+    cout<<a[n-1] - a[0] << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
